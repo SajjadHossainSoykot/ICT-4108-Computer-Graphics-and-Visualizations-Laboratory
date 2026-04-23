e8b9bcf33498f9dc8661752e11ef3cb54e03eb34
SajjadHossainSoykot <105968856+SajjadHossainSoykot@users.noreply.github.com>
2026-04-24 03:31:50 +0600

    Add Midpoint Circle Drawing Algorithm implementation with visualization

---
+++ Exp3_MidPointCircle.py
@@ -0,0 +1,51 @@
+import matplotlib.pyplot as plt
+
+def plot_circle_points(xc, yc, x, y, points):
+    symmetric_points = [
+        (xc + x, yc + y),
+        (xc - x, yc + y),
+        (xc + x, yc - y),
+        (xc - x, yc - y),
+        (xc + y, yc + x),
+        (xc - y, yc + x),
+        (xc + y, yc - x),
+        (xc - y, yc - x)
+    ]
+    points.extend(symmetric_points)
+
+def midpoint_circle(xc, yc, r):
+    x = 0
+    y = r
+    p = 1 - r
+
+    points = []
+    plot_circle_points(xc, yc, x, y, points)
+
+    while x < y:
+        x += 1
+        if p < 0:
+            p = p + 2 * x + 1
+        else:
+            y -= 1
+            p = p + 2 * x - 2 * y + 1
+        plot_circle_points(xc, yc, x, y, points)
+
+    points = list(set(points))
+    points.sort()
+
+    print("Generated Coordinates:", points)
+
+    x_coordinates = [point[0] for point in points]
+    y_coordinates = [point[1] for point in points]
+
+    plt.plot(x_coordinates, y_coordinates, marker='o', linestyle='None')
+    plt.title("Midpoint Circle Drawing Algorithm")
+    plt.xlabel("X-axis")
+    plt.ylabel("Y-axis")
+    plt.grid(True)
+    plt.axis("equal")
+    plt.savefig("Exp3_Output.png")
+    plt.show()
+
+# Example
+midpoint_circle(20, 20, 10)
